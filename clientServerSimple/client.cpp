03f1d9974f8b0ec92d1207465d682d2afdabfd9c
Muhammad Ahmed <ahmed@logicielservice.com>
2021-07-01 20:43:52 +0500

    added error handling extensively

--- clientServerSimple/client.cpp
+++ clientServerSimple/client.cpp
@@ -12,18 +12,19 @@
 
 
 
-std::string getData(boost::asio::ip::tcp::socket& socket)
+// Driver program for receiving data from buffer
+std::string getData(boost::asio::ip::tcp::socket& socket, boost::system::error_code& ec)
 {
     boost::asio::streambuf buf;
-    read_until(socket, buf, "\n");
+    boost::asio::read_until(socket, buf, "\n", ec);
     std::string data = boost::asio::buffer_cast<const char*>(buf.data());
     return data;
 }
 
-void sendData(boost::asio::ip::tcp::socket& socket, const std::string& message)
+// Driver program to send data
+void sendData(boost::asio::ip::tcp::socket& socket, const std::string& message, boost::system::error_code& ec)
 {
-    write(socket,
-          boost::asio::buffer(message + "\n"));
+    boost::asio::write(socket, boost::asio::buffer(message + "\n"), ec);
 }
 
 int main(int argc, char* argv[])
@@ -37,11 +38,20 @@ int main(int argc, char* argv[])
     // socket creation
     boost::asio::ip::tcp::socket client_socket(io_service);
 
+    boost::asio::ip::tcp::endpoint endpoint(
+        boost::asio::ip::address::from_string("127.0.0.1"), std::atoi(argv[1]));
+    
+    boost::system::error_code ec;
+
     client_socket
-        .connect(
-            boost::asio::ip::tcp::endpoint(
-                boost::asio::ip::address::from_string("127.0.0.1"),
-                std::atoi(argv[1])));
+        .connect(endpoint, ec);
+
+    if (ec)
+    {
+        std::cerr << "Failed to connect to any server at port: " << std::atoi(argv[1]) << "\n";
+
+        return -1;
+    }
 
     std::string reply, response;
 
@@ -52,7 +62,14 @@ int main(int argc, char* argv[])
 
         std::getline(std::cin, reply);
 
-        sendData(client_socket, reply);
+        sendData(client_socket, reply, ec);
+
+        if (ec)
+        {
+            std::cerr << "Failed to read/write! Something is not upto what it should be!\n";
+
+            return -1;
+        }
 
         if (reply == "exit") {
             std::cout << "End of Chat!" << "\n";
@@ -60,7 +77,14 @@ int main(int argc, char* argv[])
         }
 
         // Fetching response
-        response = getData(client_socket);
+        response = getData(client_socket, ec);
+
+        if (ec)
+        {
+            std::cerr << "Failed to read/write! Something is not upto what it should be!\n";
+
+            return -1;
+        }
 
         // removing \n
         response.pop_back();
--- clientServerSimple/server.cpp
+++ clientServerSimple/server.cpp
@@ -13,19 +13,18 @@
 
 
 // Driver program for receiving data from buffer
-std::string getData(boost::asio::ip::tcp::socket& socket)
+std::string getData(boost::asio::ip::tcp::socket& socket, boost::system::error_code& ec)
 {
     boost::asio::streambuf buf;
-    read_until(socket, buf, "\n");
+    boost::asio::read_until(socket, buf, "\n", ec);
     std::string data = boost::asio::buffer_cast<const char*>(buf.data());
     return data;
 }
 
 // Driver program to send data
-void sendData(boost::asio::ip::tcp::socket& socket, const std::string& message)
+void sendData(boost::asio::ip::tcp::socket& socket, const std::string& message, boost::system::error_code& ec)
 {
-    write(socket,
-          boost::asio::buffer(message + "\n"));
+    boost::asio::write(socket, boost::asio::buffer(message + "\n"), ec);
 }
 
 int main(int argc, char* argv[])
@@ -46,10 +45,19 @@ int main(int argc, char* argv[])
     // Creating socket object
     boost::asio::ip::tcp::socket server_socket(io_service);
 
-    // waiting for connection
-    acceptor_server.accept(server_socket);
+    boost::system::error_code ec;
 
-    std::cout << "Starting server on port: " << std::atoi(argv[1]) << "\n";
+    // waiting for connection
+    acceptor_server.accept(server_socket, ec);
+
+    if (ec)
+    {
+        std::cerr << "Failed to start server at port: " << std::atoi(argv[1]) << "\n";
+
+        return -1;
+    }
+
+    std::cout << "Started accepting at server on port: " << std::atoi(argv[1]) << "\n";
 
     // Replying with default message to initiate chat
     std::string response, reply;
@@ -57,7 +65,14 @@ int main(int argc, char* argv[])
     while (true) {
 
         // Fetching response
-        response = getData(server_socket);
+        response = getData(server_socket, ec);
+
+        if (ec)
+        {
+            std::cerr << "Failed to read/write! Something is not upto what it should be!\n";
+
+            return -1;
+        }
 
         // removing \n
         response.pop_back();
@@ -74,7 +89,14 @@ int main(int argc, char* argv[])
 
         getline(std::cin, reply);
 
-        sendData(server_socket, reply);
+        sendData(server_socket, reply, ec);
+
+        if (ec)
+        {
+            std::cerr << "Failed to read/write! Something is not upto what it should be!\n";
+
+            return -1;
+        }
 
         if (reply == "exit") {
             std::cout << "End of Chat!" << "\n";
